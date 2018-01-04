1bd9aa037e667e5925fe11b592c4547c8ed81ab5
Patrice Matz <ofjokg@gmail.com>
2018-01-04 22:54:52 +0100

    -added m-file for cramer (a22)

---
+++ Mex/a22.m
@@ -0,0 +1,9 @@
+%function accepts 1 double and 1 double matrix (#of_variables, [row1;row2])
+%each row1 contains #of_variables+1 elements
+%returns solution vector
+%bsp.:
+%			6x+12y=30
+%			3x+ 3y= 9
+%		[a1,a2]=a21(2,[6,12,30;3,3,9])
+%			a1 = 1
+%			a2 = 2
